726cfd78e5a0dcbd24266c65ba6488ba48037aad
Chris Barker <Chris.Barker@noaa.gov>
2022-01-13 18:02:12 -0800

    added a couple utilites and tests

--- libgoods/tests/test_utilities.py
+++ libgoods/tests/test_utilities.py
@@ -4,14 +4,28 @@ tests  for the libgoods.utilities package
 
 from libgoods import utilities
 
+import pytest
 
-def test_check_check_valid_latitude_valid():
-    lat = 45
 
+@pytest.mark.parametrize('lat', [45, -45, 0])
+def test_check_check_valid_latitude_valid(lat):
     assert utilities.check_valid_latitude(lat)
 
-def test_check_check_valid_latitude_invalid():
-    lat = 91
+
+@pytest.mark.parametrize('lat', [-90.0001, 90.0001])
+def test_check_check_valid_latitude_invalid(lat):
 
     with pytest.raises(ValueError):
         utilities.check_valid_latitude(lat)
+
+
+@pytest.mark.parametrize('lat', [-180, 180, 360])
+def test_check_check_valid_longitude_valid(lat):
+    assert utilities.check_valid_longitude(lat)
+
+
+@pytest.mark.parametrize('lat', [-180.001, 360.0001])
+def test_check_check_valid_longitude_invalid(lat):
+
+    with pytest.raises(ValueError):
+        utilities.check_valid_longitude(lat)
--- libgoods/utilities.py
+++ libgoods/utilities.py
@@ -4,7 +4,7 @@ assorted utilities useful for libgoods
 
 def check_valid_latitude(lat):
 
-    if -90 < lat > 90:
+    if not (-90.0 < lat < 90.0):
         raise ValueError(f'latitude cannot be larger than 90. Got{lat}')
 
     return True
@@ -23,8 +23,24 @@ def check_valid_longitude(lon, system='all'):
     COORDSYSTEMS = {'all'}
     if system not in {'all'}:
         raise ValueError(f'Invalid coordinate system. Options are: {COORDSYSTEMS}')
-    if -180 < lat > 360:
+    if not (-180.0 <= lon <= 360.0):
         raise ValueError(
             f'longitude cannot be larger than 360 or less than -180. Got{lon}')
 
     return True
+
+
+def shift_lon_coords(coords, system):
+    """
+    shift longitude to the defined system
+    """
+
+def detect_lon_coords(coords, system):
+    """
+    detects the longitude coords system
+
+     - -180 -- 180
+     - 0 -- 360
+    """
+
+
