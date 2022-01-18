fab1ea3c643efaadfde88f0d7f6c2b9b329180e1
Chris Barker <Chris.Barker@noaa.gov>
2022-01-18 10:02:11 -0800

    added a module for the core data models

---
+++ libgoods/data_models.py
@@ -0,0 +1,40 @@
+"""
+Base classes for the core data models
+"""
+
+
+class DataSource:
+    """
+    core functionality for all data sources
+    """
+    @property
+    def bounds():
+        """
+        Property that returns the bounds of the model
+
+        geo-json compatible polygon ?
+        """
+
+    @property
+    def valid_times():
+        """
+        the start and end time of the data currently available
+        """
+
+
+class Shoreline(DataSource):
+    """
+    class for acccessing maps from shoreline
+    """
+
+
+class Currents(DataSource):
+    """
+    class that represents a gridded currents
+    """
+
+
+class Winds(DataSource):
+    """
+    cclass that represents a gridded winds
+    """
