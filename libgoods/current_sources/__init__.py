f3e9395e1f9f636e43b8ccb85613e90dfa8ad4cd
amy.macfadyen <amy.macfadyen@noaa.gov>
2022-01-19 12:05:59 -0800

    Reworked TBOFS into Chris' framework

--- libgoods/current_sources/__init__.py
+++ libgoods/current_sources/__init__.py
@@ -5,6 +5,8 @@ classes for all the sources of currents
 """
 
 from .hycom import HYCOM
+from .tbofs import TBOFS
 
 all_currents = {'hycom': HYCOM(),
+                'tbofs': TBOFS(),
                 }
