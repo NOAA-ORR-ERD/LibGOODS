ee02d33ac2ae48826f198163168d42b036755464
amy.macfadyen <amy.macfadyen@noaa.gov>
2022-01-19 14:39:50 -0800

    demo of using the existing libgoods classes

--- libgoods/current_sources/hycom.py
+++ libgoods/current_sources/hycom.py
@@ -3,7 +3,7 @@ HYCOM global ocean model
 """
 import os
 from ..data_models import Currents
-from .. import rect_model, roms_model, temp_files_dir, currents_dir
+from .. import rect_model, temp_files_dir
 from ..utilities import polygon2bbox
 
 # fixme: we probably don't want actual HTML in there
@@ -16,7 +16,7 @@ For more details about the global model visit the
 <a href="http://www7320.nrlssc.navy.mil/GLBhycom1-12/prologue.html" target="_blank"> global HYCOM website</a>.",
 """
 
-class HYCOM(Currents):
+class HYCOM(rect_model.rect):
     """
     global HYCOM
     """
@@ -38,43 +38,6 @@ class HYCOM(Currents):
 
     def __init__(self):
         """ initilize a HYCOM instance"""
-        # nothing to do here
-        # but maybe in the future:
-        # ping to see if teh server is running?
-        # check if anything's chagned? bounding box, etc.
+        self.open_nc(FileName=self.url)
 
-        # other configuration?
-        pass
 
-    def get_data(self,
-                 bounds,
-                 cross_dateline,
-                 max_filesize):
-
-            '''
-            :param: bounds Sequence of (lon,lat) pairs e.g., [(lon,lat),(lon,lat)...] 
-            
-            '''
-            try:
-                bounding_box = polygon2bbox(bounds)
-            except ValueError:
-                raise NotImplementedError('Only rectangular bounds are supported')
-                
-            #bounds = [south_lat,west_lon,north_lat,east_lon]
-            url = self.url
-            var_map = self.var_map
-
-            model = rect_model.rect(url)
-
-            model.get_dimensions(var_map)
-            
-            model.subset(bounding_box)
-
-            #until I add time selection -- return last 10 time steps
-            tlen = len(model.time)
-
-            fn = self.default_filename
-            fp = os.path.join(temp_files_dir,fn)
-            model.write_nc(var_map,fp,t_index=[tlen-10,tlen,1])
-
-            return fn, fp
